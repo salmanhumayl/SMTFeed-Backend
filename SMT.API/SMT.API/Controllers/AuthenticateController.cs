b741ee4a07161f6e2c3fcc81a135e9bb516de2a8
smazhar <salman137@hotmail.com>
2023-12-20 16:23:36 +0400

    optdone

--- SMT.API/SMT.API/Controllers/AuthenticateController.cs
+++ SMT.API/SMT.API/Controllers/AuthenticateController.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using SMT.Model.Models;
 using SMT.ModelSQL.Models;
 using SMT.Service.Interface;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using VCT.API.Model;
 
 namespace SMT.API.Controllers
 {
@@ -15,10 +18,12 @@ namespace SMT.API.Controllers
     public class AuthenticateController : ControllerBase
     {
         private readonly IUsers _userservice;
+        private IWebHostEnvironment _webHostEnvironment;
 
-        public AuthenticateController(IUsers userservice)
+        public AuthenticateController(IUsers userservice, IWebHostEnvironment webHostEnvironmen)
         {
             _userservice = userservice;
+            _webHostEnvironment = webHostEnvironmen;
         }
 
         [HttpPost("UserRegistration")]
@@ -26,9 +31,26 @@ namespace SMT.API.Controllers
         {
             if (model.TwoFactorCode == null)
             {
+                string body;
+                string mailcontent;
+                string emailResponse;
                 Random rnd = new Random();
-                string randomnumber = (rnd.Next(1000, 9999).ToString());
-                return Ok(randomnumber);
+                string TwoFAtoken = (rnd.Next(1000, 9999).ToString());
+                //Send OTP Email 
+
+                EmailManager VCTEmailService = new EmailManager();
+              
+                body = VCTEmailService.GetBody(Path.Combine(_webHostEnvironment.WebRootPath, @"Template\Registration-2FA-OTP.html"));
+                mailcontent = body.Replace("@Name", model.Name);
+                mailcontent = mailcontent.Replace("@pwdchangelink", TwoFAtoken);
+                VCTEmailService.Body = mailcontent;
+           
+                VCTEmailService.Subject = "SMT OTP";
+                VCTEmailService.ReceiverAddress = model.Email;
+                VCTEmailService.ReceiverDisplayName = model.Email;
+                emailResponse = await VCTEmailService.SendEmail();
+                return Ok(TwoFAtoken);
+
             }
             else
             {
---
+++ SMT.API/SMT.API/EmailManager.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace VCT.API.Model
+{
+    public class EmailManager
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string ReceiverAddress { get; set; }
+        public string ReceiverDisplayName { get; set; }
+        public  IConfiguration Configuration { get; set; }
+        public string EnvelopID { get; set; }
+        public bool Attachement { get; set; }
+      
+           
+        public Task<string> SendEmail()
+        {
+            System.Net.Mail.MailMessage newemail = new System.Net.Mail.MailMessage();
+            System.Net.Mail.MailAddress MailReceiver = new System.Net.Mail.MailAddress(ReceiverAddress, ReceiverDisplayName);
+            System.Net.Mail.MailAddress MailSender = new System.Net.Mail.MailAddress("humaylsalman@gmail.com", "SCM");
+            newemail.From = MailSender;
+            newemail.To.Add(MailReceiver);
+            newemail.IsBodyHtml = true;
+            newemail.Subject = Subject;
+            newemail.Body = Body;
+            return SendMail(newemail);
+        }
+
+        private async Task<string> SendMail(System.Net.Mail.MailMessage MailMsg)
+        {
+            System.Net.Mail.SmtpClient newe = new System.Net.Mail.SmtpClient();
+            newe.Host = "smtp.gmail.com";
+            
+            newe.Port = 587;
+            newe.Credentials = new System.Net.NetworkCredential("humaylsalman@gmail.com", "sehprusmlyozedjm");
+          // newe.UseDefaultCredentials = true ;
+            newe.EnableSsl = true;
+            try
+            {
+                await newe.SendMailAsync(MailMsg);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+                newe.Dispose();
+            }
+            newe.Dispose();
+            return "";
+
+        }
+
+
+        public string GetBody(string TemplatePath)
+        {
+            StreamReader sr;
+            sr = new StreamReader(TemplatePath);
+            string _EmailBody = sr.ReadToEnd();
+            sr.Dispose();
+            return _EmailBody;
+        }
+    }
+}
